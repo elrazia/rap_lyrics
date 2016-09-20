1a4ef1332428f755694c7127d6b4cb544d5e88a6
Ahmed <elrazia@gmail.com>
2016-09-19 20:18:44 -0400

    docstring for ip_switcher, code cleaning

--- aws_work/ip_switcher.py
+++ aws_work/ip_switcher.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python
+
+'''
+Written to run in parallel with Tor. Script renews IP and shows new IP every 4 seconds.
+Purpose is to avoid getting kicked off of a website while scraping large amounts of data
+'''
+
 from stem import Signal
 from stem.control import Controller
 import requests
 import time
 
-# change ip
+# change IP
 def renew_connection():
 	with Controller.from_port(port = 9051) as controller:
 		controller.authenticate()
 		controller.signal(Signal.NEWNYM)
-# show ip
+# show IP
 def request():
 	proxies = {'http':'socks5://127.0.0.1:9050', 'https':'socks5://127.0.0.1:9050'}
 	print requests.get('http://icanhazip.com',proxies=proxies).text.strip()
