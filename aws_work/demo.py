0f75192419c0d726ba0816a7cc6a5558732aecef
Ahmed <elrazia@gmail.com>
2016-09-19 20:00:13 -0400

    Update demo.py

--- aws_work/demo.py
+++ aws_work/demo.py
@@ -3,11 +3,12 @@ from stem.control import Controller
 import requests
 import time
 
+# change ip
 def renew_connection():
 	with Controller.from_port(port = 9051) as controller:
 		controller.authenticate()
 		controller.signal(Signal.NEWNYM)
-
+# show ip
 def request():
 	proxies = {'http':'socks5://127.0.0.1:9050', 'https':'socks5://127.0.0.1:9050'}
 	print requests.get('http://icanhazip.com',proxies=proxies).text.strip()
